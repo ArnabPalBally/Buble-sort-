f55e07884745425f8838085deedea21b66d2e874
ArnabPalBally <77872764+ArnabPalBally@users.noreply.github.com>
2021-01-26 10:02:43 +0530

    Add files via upload

---
+++ buble sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void print(int arr[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
+int main()
+{
+	int arr[]={64,34,25,12,22,11,9};
+	int n =sizeof(arr)/sizeof(arr[0]);
+	int i,j;
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-1-i;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	printf("the sorted array:");
+	print(arr,n);
+}
